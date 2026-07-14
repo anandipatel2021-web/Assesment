b72e55d105356ace5855392982ff03bac8e14443
Anandi Makadiya <anandipatel2021@gmail.com>
2026-07-14 12:59:49 +0530

    Create 1.py

---
+++ Python/1.py
@@ -0,0 +1,70 @@
+print('1. Basic Arithmatic Operation')
+a= 25
+b= 7
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+print('-------------------------------------------------')
+
+
+print('2. Rectangle Area Comparison')
+length1 = 12
+breadth1 = 8
+
+length2 = 15
+breadth2 = 6
+
+print(length1*breadth1)
+print(length2*breadth2)
+print('-------------------------------------------------')
+
+
+print('3. Even or Odd')
+
+num = 57
+
+if(num%2): 
+    print('Odd')
+else:
+    ('Even')
+
+print('-------------------------------------------------')
+
+
+print('4. Swap Numbers')
+
+x = 45
+y = 18
+
+x=x+y
+y=x-y
+x=x-y
+
+print(f' y is {y}')
+print(f' x is {x}')
+
+print('-------------------------------------------------')
+
+print('5. Student Eligibility')
+
+age = 19
+percentage = 72
+
+if (age >= 17) & (percentage >= 60):
+    print('Eligible')
+else:
+     print('Not Eligible')
+ 
+print('-------------------------------------------------') 
+
+print('6. Largest of Three Numbers')
+
+a = 78
+b = 92
+c = 65
+
